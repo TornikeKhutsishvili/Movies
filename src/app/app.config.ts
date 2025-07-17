4f3c1c7aa96476f30bf1ccfaaf615970690ec40f
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2025-07-17 23:00:28 +0400

    Modify server ts, app config ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination'
 
 export const appConfig: ApplicationConfig = {
+
   providers: [
     provideHttpClient(withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    NgxPaginationModule,
   ]
+
 };
