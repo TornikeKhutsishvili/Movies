3db899071adcf4494df6b9905e9ea76e567e6ec5
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2025-07-18 00:25:04 +0400

    Modify routes server ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,41 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'movie-list/movie-details/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const apiKeys = [
+        't0XNBuNEaL4lMfCVvx90ks41SrlQlWynqX5gqGB3',
+        'EFCY2h2VghuMcuVW60TvMyWN9glnfkDhg1QKgvrk',
+        'nYKWjq7aJRd5Q8xKkUyFSGGtMPfuBO2JCk8OUia8',
+      ];
+
+      const baseUrl = 'https://api.watchmode.com/v1';
+      const endpoint = '/list-titles/?types=movie&sort_by=release_date_desc&limit=60';
+
+      let data: any = null;
+
+      for (let i = 0; i < apiKeys.length; i++) {
+        const url = `${baseUrl}${endpoint}&apiKey=${apiKeys[i]}`;
+        try {
+          const response = await fetch(url);
+          if (!response.ok) continue;
+
+          data = await response.json();
+          break;
+        } catch (error) {
+          console.error(`API Key ${i} failed`, error);
+        }
+      }
+
+      if (!data?.titles) return [];
+
+      return data.titles.map((title: any) => ({
+        id: title.id.toString()
+      }));
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
